f6087e2adf159ed03df5b042a31c74f51a4317e5
Gard <gaf1902@outlook.com>
2023-09-13 15:13:49 +0200

    added router and empty index pages for all pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-import { useState } from 'react'
+import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Profile from './pages/profile';
+import Login from './pages/login';
+import Translation from './pages/translation';
+import Error from './pages/error';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/translate"
+          element={<Translation/>}/>
+          <Route
+           path= "/login"
+           element={<Login/>}
+           /> 
+           <Route
+           path= "/profile"
+           element={<Profile/>}
+           /> 
+           {/* TODO NAVIGATE TO LOGIN */}
+           <Route
+           path= "/"
+           element={<p>ll</p>}
+           /> 
+           <Route
+           path= "/error"
+           element={<Error/>}
+           /> 
+      </Routes>
+  </BrowserRouter>
   )
 }
 
---
+++ src/pages/translation/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Translation = () => {
+  return (
+    <div>translate</div>
+  )
+}
+
+export default Translation
