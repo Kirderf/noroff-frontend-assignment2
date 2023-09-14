5b0424241a8547650631d79a1b585f842fe0c1c3
Haukaun <haakonfs@hotmail.com>
2023-09-14 12:44:36 +0200

    add: transelatePage

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import InputField from '../../components/InputField'
 
-
 const Profile = () => {
   return (
-    <div><InputField placeHolder={"Tst"} /></div>
+    <div></div>
   )
 }
 
