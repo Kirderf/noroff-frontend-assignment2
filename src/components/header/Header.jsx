ab465d731021f40f0649ab5982fb70a4be5367e9
Gard <gaf1902@outlook.com>
2023-09-14 14:25:37 +0200

    fixed error with header in app, change bordercolor

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import IconWithTitle from '../iconTitle/IconWithTitle'
 const Header = () => {
   return (
-    <header class="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full text-sm py-4 dark:bg-gray-800 bg-yellow border-b-4 border-r-yellow-dark">
+    <header class="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full text-sm py-4 dark:bg-gray-800 bg-yellow border-b-4 border-b-gray">
       <nav class="flex flex-nowrap  max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
         <IconWithTitle />
         <div id="navbar-image-and-text-2" class="grow block gap-2">
@@ -12,7 +12,7 @@ const Header = () => {
           </div>
         </div>
       </nav>
-      <br></br>
+      
     </header>
   )
 }
