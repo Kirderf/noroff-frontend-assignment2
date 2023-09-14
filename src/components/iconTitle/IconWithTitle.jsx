d2b5d7e0bb2218207d996313bb710c03b62111e6
Haukaun <haakonfs@hotmail.com>
2023-09-14 15:50:41 +0200

    add: loginpage

--- src/components/iconTitle/IconWithTitle.jsx
+++ src/components/iconTitle/IconWithTitle.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const IconWithTitle = () => {
   return (
-    <div class=" flex gap-6 items-center text-3xl font-semibold hs-collapse transition-all duration-300 basis-full ">
+    <div className=" flex gap-6 items-center text-3xl font-semibold hs-collapse transition-all duration-300 basis-full ">
       <div>
         <img
-          class="relative bg-[url('./src/assets/Backgroundicon.svg')] bg-[10%_100%] w-20 max-w-3xl bottom-0 px-3 py-1"
+          className="relative bg-[url('./src/assets/Backgroundicon.svg')] bg-[10%_100%] w-20 max-w-3xl bottom-0 px-3 py-1"
           style={{
             backgroundSize: "100% 120%",
             backgroundPositionY: "-12px",
@@ -16,7 +16,7 @@ const IconWithTitle = () => {
         />
       </div>
       <h1
-        class="text-white font-light"
+        className="text-white font-light"
         style={{
           fontFamily: "'Love Ya Like A Sister', cursive",
         }}
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,19 +1,44 @@
 import React from "react";
 import InputField from "../../components/InputField";
+import IconWithTitle from "../../components/iconTitle/IconWithTitle";
 
 const Login = () => {
   return (
     <section className="flex flex-col relative">
-      <div className=" bg-yellow w-full h-96 flex items-center justify-center">
-        <div className="centering"></div>
-      </div>
-      <div className="centering">
-        <div className="shadow-2xl rounded-2xl h-auto w-full flex flex-col justify-center items-start">
-          <div className="flex flex-wrap h-auto p-10 w-full">
-            <InputField placeHolder={"Any translaterino?"} />
+      <div className=" bg-yellow w-full h-72 flex items-center justify-center">
+        <div className="centering">
+          <div className="flex">
+            <img
+              className="relative bg-[url('./src/assets/Backgroundicon.svg')] bg-[10%_100%] w-64 max-w-3xl bottom-0 px-3 py-1"
+              style={{
+                backgroundSize: "100% 120%",
+                backgroundPositionY: "-12px",
+                backgroundRepeat: "no-repeat",
+              }}
+              src="./src/assets/Logo.png"
+              alt="Logo"
+            />
+            <div
+              className="flex flex-col text-5xl pl-10 justify-center text-white font-light"
+              style={{
+                fontFamily: "'Love Ya Like A Sister', cursive",
+              }}
+            >
+              <h1>Lost in Translation</h1>
+              <h2>Get Started</h2>
+            </div>
           </div>
-          <div className="w-full bg-purple rounded-b-2xl h-12 flex justify-start items-center">
-            <div className="w-auto m-5 bg-white rounded-full"></div>
+        </div>
+      </div>
+      <div className="bg-yellow w-full h-24">
+        <div className="centering">
+          <div className="shadow-2xl rounded-2xl h-auto w-full flex flex-col justify-center items-start bg-white">
+            <div className="flex flex-wrap h-auto p-10 w-full">
+              <InputField placeHolder={"Username:"} />
+            </div>
+            <div className="w-full bg-purple rounded-b-2xl h-12 flex justify-start items-center">
+              <div className="w-auto m-5 bg-white rounded-full"></div>
+            </div>
           </div>
         </div>
       </div>
