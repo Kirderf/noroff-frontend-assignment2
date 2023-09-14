966dae9d89c3d00f38e483f1d9eef77616fdb6fe
Haukaun <haakonfs@hotmail.com>
2023-09-14 15:04:14 +0200

    fix: header with specified margin

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import IconWithTitle from '../iconTitle/IconWithTitle'
+import React from "react";
+import IconWithTitle from "../iconTitle/IconWithTitle";
 const Header = () => {
   return (
-    <header class="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full text-sm py-4 dark:bg-gray-800 bg-yellow border-b-4 border-b-gray">
-      <nav class="flex flex-nowrap  max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
-        <IconWithTitle />
-        <div id="navbar-image-and-text-2" class="grow block gap-2">
-          <div class="pr-20 flex mt-5 flex-row items-center justify-end sm:mt-0 sm:pl-5">
-            <div class=" bg-yellow-dark rounded-2xl pr-14 pl-5 py-0.5"><p class="text-white">Gard</p></div>
-            <img class="w-12 h-auto absolute  bg-yellow-dark rounded-full p-0" src="./src/assets/profileUser.svg" alt="Logo" href="profile"/>
+    <header className="flex flex-wrap z-50 w-full text-sm py-4 bg-yellow border-b-4 border-b-gray">
+      <div className="centering">
+        <nav
+          class="flex flex-nowrap  max-w-[85rem] w-full sm:flex sm:items-center sm:justify-between"
+          aria-label="Global"
+        >
+          <IconWithTitle />
+          <div id="navbar-image-and-text-2" className="grow block gap-2">
+            <div className="flex flex-row items-center justify-end">
+              <div className=" bg-yellow-dark rounded-2xl pr-14 pl-5 py-0.5">
+                <p className="text-white">Gard</p>
+              </div>
+              <img
+                className="w-12 h-auto absolute  bg-yellow-dark rounded-full"
+                src="./src/assets/profileUser.svg"
+                alt="Logo"
+                href="profile"
+              />
+            </div>
           </div>
-        </div>
-      </nav>
-      
+        </nav>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
