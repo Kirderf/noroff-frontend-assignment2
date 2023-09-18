7398c9dfb1c7b8f208c28cade4311d4a7a809f34
Gard <gaf1902@outlook.com>
2023-09-18 09:38:54 +0200

    fixed centering on error img

--- src/pages/error/index.jsx
+++ src/pages/error/index.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Error = (props) => {
   const url = "https://http.cat/" + props.code
   return (
-    <div>
-      <img src={url}/>
+    <div className='min-w-max centering flex justify-center'>
+      <img className="" src={url}/>
     </div>
   )
 }
