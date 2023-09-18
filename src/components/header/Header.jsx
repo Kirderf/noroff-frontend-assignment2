3d56a4276f21f1f76095996b17963f7ba9c6f767
Gard <gaf1902@outlook.com>
2023-09-18 09:25:38 +0200

    added navigate to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import IconWithTitle from "../iconTitle/IconWithTitle";
 import { useUser } from "../../contexts/UserProvider";
+import { useNavigate  } from "react-router-dom";
 
 const Header = () => {
   const { user, setUser } = useUser();
+  const navigate = useNavigate();
   return (
     <header className="flex flex-wrap z-50 w-full text-sm py-4 bg-yellow border-b-4 border-b-gray">
       <div className="centering">
@@ -11,9 +13,12 @@ const Header = () => {
           className="flex flex-nowrap w-full sm:flex sm:items-center sm:justify-between"
           aria-label="Global"
         >
+          <div onClick={() => navigate("/")}>
           <IconWithTitle />
+          </div>
+          
           <div id="navbar-image-and-text-2" className="grow block gap-2">
-              <div className="flex flex-row items-center justify-end">
+              <div className="flex flex-row items-center justify-end" onClick={() => navigate("/profile")}>
               {user?.user?.username && 
               <div className=" bg-yellow-dark rounded-2xl pr-14 pl-5 py-0.5">
                 <p className="text-white">{user?.user?.username}</p>
